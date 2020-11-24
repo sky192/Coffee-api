5bdc8f0bf0d111bd918b077e8d815b28d69c31ba
sky192 <samahehloghmani@gmail.com>
2020-11-24 11:46:35 -0500

    create pug

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express')
+
+const states = require('./states')
 const bodyParser = require('body-parser')
 
 const getAllStates = require('./controllers/states')
 
 const app = express()
 
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+    return res.render('index', {states})
+})
+
+
 app.get('/', getAllStates)
 
+
+app.all('*', (req,res) => {
+    return res.sendStatus(404)
+}
+)
 
 app.listen(1337, () => {
     console.log('listening on port 1337...')
