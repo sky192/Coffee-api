fc08882462d7f0dd4fca5eab6d47863a30284443
sky192 <samahehloghmani@gmail.com>
2020-11-24 14:11:23 -0500

    add some functions

--- controllers/states.js
+++ controllers/states.js
@@ -1,15 +1,19 @@
 const states = require('../states')
+
 const getAllStates = (req, res) => {
-    return res.send(states)
+    return res.render('index', {states})
+}
+
+
+const getStateByCapital = (req, res) => {
+    const { capital } = req.params
+
+    const foundState = states.filter((state) =>  state.capital === capital)
+
+    return res.send(foundState)
 }
 
 
 
 
-
-
-
-
-
-
-module.exports = getAllStates
+module.exports = { getAllStates, getStateByCapital }
--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 const express = require('express')
 
-const states = require('./states')
-const bodyParser = require('body-parser')
 
-const getAllStates = require('./controllers/states')
+const { getAllStates, getStateByCapital } = require('./controllers/states')
 
 const app = express()
 
 app.set('view engine', 'pug')
-
-app.get('/', (req, res) => {
-    return res.render('index', {states})
-})
-
+app.use(express.static('public'))
 
 app.get('/', getAllStates)
+
+app.get('/:capital', getStateByCapital)
 
 
 app.all('*', (req,res) => {
     return res.sendStatus(404)
-}
-)
+})
 
 app.listen(1337, () => {
     console.log('listening on port 1337...')
