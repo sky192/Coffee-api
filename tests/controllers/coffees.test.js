fcd696d8d4396d33998a4180575788fb97e6ced4
sky192 <samahehloghmani@gmail.com>
2020-12-21 20:41:25 -0500

    create test on getcoffeeByTitle() and saveNewCoffee()

--- tests/controllers/coffees.test.js
+++ tests/controllers/coffees.test.js
@@ -1,11 +1,12 @@
 /* eslint-disable linebreak-style */
+
 const chai = require('chai')
 const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
 const models = require('../../models')
 const { describe, it } = require('mocha')
-const { coffeesList } = require('../mocks/coffees')
-const { getAllCoffees } = require('../../controllers/coffee')
+const { coffeesList, singleCoffee } = require('../mocks/coffees')
+const { getAllCoffees, getCoffeeByTitle, saveNewCoffee } = require('../../controllers/coffee')
 
 chai.use(sinonChai)
 const { expect } = chai
@@ -26,8 +27,36 @@ describe('Controllers - coffee', () => {
       })
   })
 
-  describe('getCoffeeByTitle', () => {})
+  describe('getCoffeeByTitle', () => {
+    it('retrieves the coffee associated with the provided title from the database and calls response.send with it',
+      async () => {
+        const request = { params: { title: 'Cappuccino' } }
+        const stubbedSend = sinon.stub()
+        const response = { send: stubbedSend }
+        const stubbedFindOne = sinon.stub(models.Coffees, 'findOne').returns(singleCoffee)
 
-  describe('saveNewCoffee', () => {})
+        await getCoffeeByTitle(request, response)
+
+        expect(stubbedFindOne).to.have.been.calledWith({ where: { title: 'cappucino' } })
+        expect(stubbedSend).to.have.been.calledWith(singleCoffee)
+      })
+  })
+
+  describe('saveNewCoffee', () => {
+    it('accepts new coffee details and saves them as a new coffee, returning the saved record with a 201 status',
+      async () => {
+        const request = { body: singleCoffee }
+        const stubbedSend = sinon.stub()
+        const stubbedStatus = sinon.stub().returns({ send: stubbedSend })
+        const response = { status: stubbedStatus }
+
+        const stubbedCreate = sinon.stub(models.Coffees, 'create').returns(singleCoffee)
+
+        await saveNewCoffee(request, response)
+
+        expect(stubbedCreate).to.have.been.calledWith(singleCoffee)
+        expect(stubbedStatus).to.have.been.calledWith(201)
+        expect(stubbedSend).to.have.been.calledWith(singleCoffee)
+      })
+  })
 })
-
--- tests/mocks/coffees.js
+++ tests/mocks/coffees.js
@@ -10,4 +10,10 @@ const coffeesList = [{
 
 }]
 
-module.exports = { coffeesList }
+const singleCoffee = {
+  title: 'Cappuccino',
+  description: 'Cappuccino is a latte made with more foam than steamed milk, often with a sprinkle of cocoa powder or cinnamon on top. Sometimes you can find variations that use cream instead of milk or ones that throw in flavor shot, as well.',
+}
+
+
+module.exports = { coffeesList, singleCoffee }
